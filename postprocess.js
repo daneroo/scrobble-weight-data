e77999493105758fad6a1dc270011b80df4fc652
Daniel Lauzon <daniel.lauzon@gmail.com>
2021-09-22 01:09:47 -0400

    single data formatted output

--- postprocess.js
+++ postprocess.js
@@ -7,19 +7,13 @@ const json = await readJSON(filename)
 const {_id, values} = json
 console.log(`fetched data for ${json._id}: (${values.length})`)
 
-const imperialValues = values.map(({value,stamp}) => ({ 
+const formattedValues = values.map(({value,stamp}) => ({ 
   stamp,
-  lbs: Number(value/1000).toFixed(1)
-}));
-await writeJSON('imperial.json', imperialValues) // create a new JSON file with just the Bitcoin price
-console.log("Wrote imperial values")
-
-const metricValues = values.map(({value,stamp}) => ({ 
-  stamp,
+  lbs: Number(value/1000).toFixed(1),
   kg: Number(value/2205).toFixed(1)
 }));
-await writeJSON('metric.json', imperialValues) // create a new JSON file with just the Bitcoin price
-console.log("Wrote metric values")
+await writeJSON('formatted.json', formattedValues) // create a new JSON file with just the Bitcoin price
+console.log("Wrote formatted values")
 
 // Optionally delete the original file
 // await removeFile(filename)
